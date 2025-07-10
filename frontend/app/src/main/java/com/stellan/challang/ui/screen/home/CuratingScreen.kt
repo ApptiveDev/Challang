6feaac435b14d51e0149a3371a461b929854c90e
FiveStarK <gbkos@naver.com>
2025-07-10 17:33:28 +0900

    큐레이팅 스크린 구성 완성.

--- frontend/app/src/main/java/com/stellan/challang/ui/screen/home/CuratingScreen.kt
+++ frontend/app/src/main/java/com/stellan/challang/ui/screen/home/CuratingScreen.kt
@@ -5,27 +5,34 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Download
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.outlined.Share
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.SearchBar
 import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.SuggestionChip
 import androidx.compose.material3.SuggestionChipDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -44,6 +51,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.stellan.challang.R
 import com.stellan.challang.hasSeenGuideFlow
 import com.stellan.challang.rememberRecentSearches
@@ -192,21 +200,96 @@ fun CuratingScreen(onDetail: (drinkID: String) -> Unit) {
                     .padding(27.dp)
                     .background(brush = Brush.verticalGradient(
                         colors = listOf(Color(0xFF6CD0D8), Color.White)
-                    ), shape = RoundedCornerShape(12.dp)),
+                    ), shape = RoundedCornerShape(24.dp)),
                 colors = CardDefaults.cardColors(
                     containerColor = Color.Transparent
                 ),
-                shape = RoundedCornerShape(12.dp),
+                shape = RoundedCornerShape(24.dp),
             ) {
                 Box(Modifier.fillMaxSize()) {
                     Image(
                         painter = painterResource(R.drawable.balvenie),
                         contentDescription = null,
-                        contentScale = ContentScale.Fit,
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(20.dp)
+                        contentScale = ContentScale.FillWidth,
+                        modifier = Modifier.fillMaxWidth().padding(top = 40.dp)
                     )
+
+                    Column(
+                        modifier = Modifier
+                            .align(Alignment.TopStart)
+                            .padding(top = 40.dp, start = 24.dp)
+                    ) {
+                        Text(
+                            text = "발베니",
+                            fontFamily = PaperlogyFamily,
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 34.sp,
+                            color = Color.White
+                        )
+                        Text(
+                            text = "40%",
+                            fontFamily = PaperlogyFamily,
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 34.sp,
+                            color = Color.White
+                        )
+                        Text(
+                            text = "고도수",
+                            fontFamily = PaperlogyFamily,
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 34.sp,
+                            color = Color.White
+                        )
+                    }
+
+                    Row(
+                        modifier = Modifier
+                            .align(Alignment.BottomStart)
+                            .fillMaxWidth()
+                            .padding(bottom = 15.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Spacer(modifier = Modifier.weight(1f))
+                        Box(
+                            modifier = Modifier.weight(1.3f),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            TextButton(
+                                onClick = { onDetail("1") },
+                                colors = ButtonDefaults.buttonColors(
+                                    containerColor = Color(0xFF98DFE5)
+                                ),
+                                modifier = Modifier.size(width = 180.dp, height = 40.dp)
+                            ) {
+                                Text(
+                                    "자세히 보기",
+                                    fontFamily = PaperlogyFamily,
+                                    fontWeight = FontWeight.Bold,
+                                    fontSize = 18.sp,
+                                    color = Color.White
+                                )
+                            }
+                        }
+                        Row(
+                            modifier = Modifier.weight(1f),
+                            horizontalArrangement = Arrangement.Center
+                        ) {
+                            IconButton(onClick = { /* 공유 */ }) {
+                                Icon(Icons.Outlined.Share,
+                                    contentDescription = "공유하기",
+                                    tint = Color(0xFF6CD0D8),
+                                    modifier = Modifier
+                                        .size(35.dp))
+                            }
+                            IconButton(onClick = { /* 다운로드 */ }) {
+                                Icon(Icons.Default.Download,
+                                    contentDescription = "다운로드",
+                                    tint = Color(0xFF6CD0D8),
+                                    modifier = Modifier
+                                        .size(35.dp))
+                            }
+                        }
+                    }
                 }
             }
         }
